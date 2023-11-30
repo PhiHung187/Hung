0d6de7f1b5d5425ac823e3fcede9cfe36eff7da2
PhiHung187 <151615882+PhiHung187@users.noreply.github.com>
2023-11-30 19:47:28 +0700

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+
+// import AccountModel
+const CustomerSchema  = require('./models/module.js')
+// const path = require('path')
+const jwt = require('jsonwebtoken')
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(bodyParser.json({ limit: '10mb' }));
+
+
+// app.use('/public', express.static(path.join( '/public')))
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
+
+
+app.use('/api/admin', require('./api/admin.js'));
+app.use('/api/customer', require('./api/customer.js'));
+
+
+var path = require('path');
+// console.log('__dirname',__dirname)
+// '/admin' serve the files at client-admin/build/* as static files
+app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
+app.get('admin/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
+});
+// '/' serve the files at client-customer/build/* as static files
+app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}/home`);
+  });
---
+++ server/untils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'ecommerce.ydmk0xv.mongodb.net', 
+    DB_USER: 'nguyenconghieu05092019',
+    DB_PASS: 'buaconhachuc1',
+    DB_DATABASE: 'ecommerce',
+    EMAIL_USER: 'nguyenconghieu05092019@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'buaconhachuc1',
+    JWT_SECRET: 'hieu',
+    JWT_EXPIRES: '6000000000', // in milliseconds
+  };
+module.exports = MyConstants;
